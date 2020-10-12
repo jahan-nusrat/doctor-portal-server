10522354b9133f6d91387eaa80ef739c813f704c
Nusrat Jahan <njnusrat53@gmail.com>
2020-10-12 19:15:51 +0600

    add booking

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const express = require('express')
 const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongodb = require('mongodb');
 const MongoClient = require('mongodb').MongoClient;
-require('dotenv').config();
+
 
 const app = express()
 app.use(cors());
@@ -19,29 +20,29 @@ app.get('/', (req, res) => {
 })
 
 const uri = "mongodb+srv://doctorsportal:adgjmptw499@cluster0.oawso.mongodb.net/doctors-portal?retryWrites=true&w=majority";
+/* const uri =  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oawso.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`; */
+
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
+client.connect(err => {
+    const appointmentCollection = client.db("doctors-portal").collection("appointments");
+    const bookingsCollection = client.db("doctors-portal").collection("bookingsData");
 
-//Add services to database
-app.post("/addServices", (req, res) => {
-    const services = req.body;
-    client.connect(err => {
-        const collection = client.db("doctors-portal").collection("appointments");
-        collection.insertMany(services)
+    //Add services to database
+    app.post("/addServices", (req, res) => {
+        const services = req.body;
+        appointmentCollection.insertMany(services)
             .then((result) => {
                 res.send(result)
-            })
-    });
+        })
 })
 
-//get service data from the server
-app.get("/getServices", (req, res) => {
-    client.connect((error) => {
-        const collection = client.db("doctors-portal").collection("appointments");
-        collection.find().toArray((err, documents) => {
+    //get service data from the server
+    app.get("/getServices", (req, res) => {
+        appointmentCollection.find().toArray((err, documents) => {
             if (err) {
                 console.log(err);
                 console.log(error)
@@ -53,8 +54,17 @@ app.get("/getServices", (req, res) => {
             }
         });
     });
-});
 
+    //Add appointment data
+    app.post('/addBooking',(req, res)=>{
+        const booking=req.body
+        bookingsCollection.insertOne(booking)
+        .then(result=>{
+            res.send(result.insertedCount>0)
+        })
+    })
+
+});
 
 const PORT = process.env.PORT || 5000;
 
